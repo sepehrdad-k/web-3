a856e9d5bfcc8a1048959c05d6fd103c010f9e7c
sepehrdad <sepehrdad_kashiri@yahoo.com>
2021-01-05 05:08:51 -0800

    web---3

---
+++ Web3-find index/indexfi.js
@@ -0,0 +1,7 @@
+let arr = [10,11,12,13,14,15,16,17,18,19]
+function x(y){
+    return y>14
+
+}
+
+console.log(arr.findIndex(x))
---
+++ Web3-for each/indexfe.js
@@ -0,0 +1,6 @@
+let arr = [1,2,3,4,5,6,7,8,9]
+function x(y){
+    y=y*5
+    console.log(y)
+}
+arr.forEach(x)
