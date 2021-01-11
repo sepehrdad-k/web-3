d06ee1b8b569fcd21bcc94cf4ddae51c42372e29
sepehrdad <sepehrdad_kashiri@yahoo.com>
2021-01-11 02:07:37 -0800

    teal for life

--- Web3-includes/indexin.js
+++ Web3-includes/indexin.js
@@ -1,4 +1,4 @@
 let a= "kababs are superior to pizzas.this is a fact"
-console.log(a.includes(teal))
-console.log(a.includes(kababs))
+console.log(a.includes("teal"))
+console.log(a.includes("kababs"))
 console.log(a)
---
+++ Web3-index of/indexio.js
@@ -0,0 +1,3 @@
+let a = "teal is the best color in the world"
+console.log(a.indexOf("color"))
+console.log(a)
