19f9065e8ca6501452251df9797a08158e4b7793
sepehrdad <sepehrdad_kashiri@yahoo.com>
2021-01-23 03:22:57 -0800

    sub str and starts with

---
+++ Web3-starts with/indexsw.js
@@ -0,0 +1,6 @@
+let str="hello world i'm sepehr i'm op"
+let x= str.startsWith("hello")
+let y= str.startsWith("sepehr")
+console.log(str)
+console.log(x)
+console.log(y)
---
+++ Web3-substr/indexsu.js
@@ -0,0 +1,4 @@
+let str="hello world i'm sepehr"
+let x=str.substr(2,5)
+console.log(x)
+console.log(str)
