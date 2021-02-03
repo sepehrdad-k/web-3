4131a3236539ee0b44b87016a4c0f12e3df9c8f2
sepehrdad <sepehrdad_kashiri@yahoo.com>
2021-02-03 04:24:34 -0800

    task 7

--- Web3-task7/indext7.js
+++ Web3-task7/indext7.js
@@ -7,6 +7,13 @@ let form=document.getElementById("form")
 let title=document.getElementById("title")
 let body=document.getElementById("body")
 let userid=document.getElementById("userid")
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(function(result){
+    return result.json()
+  })
+  .then(function(result2){
+    console.log(result2)
+  });
 function info(evt){
     evt.preventDefault()
     payload.title=title.value
@@ -19,8 +26,13 @@ function info(evt){
     'Content-type': 'application/json; charset=UTF-8',
   },
 })
-.then((response) => response.json())
-  .then((json) => console.log(json));
+.then(function(result){
+  return result.json()
+})
+.then(function(result2){
+  console.log(result2)
+});
+
 }
 form.addEventListener("submit",info)
 
