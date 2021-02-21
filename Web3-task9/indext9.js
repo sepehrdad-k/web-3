800bfc55ef813704b2161211750ecdeff0c9c0af
sepehrdad <sepehrdad_kashiri@yahoo.com>
2021-02-21 05:24:18 -0800

    task9

--- Web3-task9/indext9.js
+++ Web3-task9/indext9.js
@@ -1,5 +1,6 @@
 $("#form").on("submit",puttask)
 $("#btn").on("click",puttask)
+$("#btn2").on("click",savetasks)
 let keys=Object.values(localStorage)
 for(let k=0;k<keys.length;k++){
     let task=$("<li></li>")
@@ -13,7 +14,6 @@ function puttask(evt){
     evt.preventDefault()
     let task=$("<li></li>")
     keys.push($("#task_input").val())
-    localStorage.setItem($("#task_input").val()+"delete",$("#task_input").val())
     task.text($("#task_input").val())
     $("#task_input").val("")
     let btn_dlt=$('<button type="button" class="dlt">delete</button>')
@@ -25,4 +25,9 @@ function dlttask(){
     let pr=$(this).parent()
     pr.remove()
     localStorage.removeItem(pr.text())
+}
+function savetasks(){
+    for(let i=0;i<keys.length;i++){
+        localStorage.setItem(keys[i]+"delete",keys[i])
+    }
 }
